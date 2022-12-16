0cecc9e5900199bafb83b2b850f80fb6c8fb45be
antonioflore <floreantonio@hotmail.it>
2022-12-16 01:13:26 +0100

    create-smart-building

---
+++ hooks/Web3Client.js
@@ -0,0 +1,52 @@
+import StoreBuild from "../truffle/build/contracts/Struct.json";
+import Web3 from "web3";
+let selectedAccount;
+let storeContract;
+let isInitialized = false;
+
+export const init = async () => {
+  let provider = window.ethereum;
+
+  if (typeof provider !== "undefined") {
+    provider
+      .request({ method: "eth_requestAccounts" })
+      .then((accounts) => {
+        selectedAccount = accounts[0];
+        console.log(`Selected account is ${selectedAccount}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    window.ethereum.on("accountsChanged", function (accounts) {
+      selectedAccount = accounts[0];
+      console.log(`Selected account changed to ${selectedAccount}`);
+    });
+  }
+  const web3 = new Web3(provider);
+  const networkId = await web3.eth.net.getId();
+  storeContract = new web3.eth.Contract(
+    StoreBuild.abi,
+    StoreBuild.networks[networkId].address
+  );
+  isInitialized = true;
+};
+
+export async function setHash(value) {
+  if (!isInitialized) {
+    await init();
+  }
+
+  return storeContract.methods
+    .setRoomInfo(2, "test", "test")
+    .send({ from: selectedAccount });
+}
+
+export async function readHash() {
+  if (!isInitialized) {
+    await init();
+  }
+
+  return storeContract.methods.getRoomInfo().call((err, result) => {
+    console.log("stored cid in smart contract:", result);
+  });
+}
